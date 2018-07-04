6ff6af32714c3a221b8e36465b3fac757880e506
sonnvpc <sonnv6460@co-well.com.vn>
2018-07-04 18:28:49 +0700

    sonnv fix adminmenu api

--- ApiBase/Controllers/AdminMenuController.cs
+++ ApiBase/Controllers/AdminMenuController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using ApiBase.Model;
 using ApiBase.Models.BusinessAccess;
 using ApiBase.Models.DB;
