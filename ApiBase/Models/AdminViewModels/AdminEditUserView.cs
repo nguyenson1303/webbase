a346840cd152b649a6e0da37e4aa369c31c97bfb
sonnvpc <sonnv6460@co-well.com.vn>
2018-07-06 15:38:00 +0700

    sonnv add view

---
+++ ApiBase/Models/AdminViewModels/AdminEditUserView.cs
@@ -0,0 +1,56 @@
+﻿namespace Web.Areas.Admin.ViewModels
+{
+    /// <summary>
+    /// user view
+    /// </summary>
+    public class AdminEditUserView
+    {
+        /// <summary>
+        /// Gets or sets the username.
+        /// </summary>
+        /// <value>
+        /// The username.
+        /// </value>
+        public string Username
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the Internet Protocol address.
+        /// </summary>
+        /// <value>
+        /// The Internet Protocol address.
+        /// </value>
+        public string Ip
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="User_view"/> is online.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if online; otherwise, <c>false</c>.
+        /// </value>
+        public bool Online
+        {
+            get;
+            set;
+        }
+       
+        /// <summary>
+        /// Gets or sets the role.
+        /// </summary>
+        /// <value>
+        /// The role.
+        /// </value>
+        public int Role
+        {
+            get;
+            set;
+        }  
+    }
+}
---
+++ ApiBase/Models/DB/UserPageAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiBase.Models.DB
+{
+    public partial class UserPageAction
+    {
+        public int Id { get; set; }
+        public string ActionName { get; set; }
+        public string ActionDescription { get; set; }
+        public int? ActionStatus { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? ModifyDate { get; set; }
+    }
+}
