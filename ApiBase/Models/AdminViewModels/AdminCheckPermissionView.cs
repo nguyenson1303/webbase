da7121fa38b95a2ec5112c04ab28ed703d7f72f2
sonnvpc <sonnv6460@co-well.com.vn>
2018-07-16 09:49:16 +0700

    sonvn add file

---
+++ ApiBase/Models/AdminViewModels/AdminCheckPermissionView.cs
@@ -0,0 +1,44 @@
+﻿namespace ApiBase.Models.AdminViewModels
+{
+    /// <summary>
+    /// user view
+    /// </summary>
+    public class AdminCheckPermissionView
+    {
+        /// <summary>
+        /// Gets or sets the Path.
+        /// </summary>
+        /// <value>
+        /// The Path.
+        /// </value>
+        public string Path
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the TypeAct.
+        /// </summary>
+        /// <value>
+        /// The TypeAct.
+        /// </value>
+        public string TypeAct
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the Type.
+        /// </summary>
+        /// <value>
+        /// The confirm Type.
+        /// </value>
+        public string Type
+        {
+            get;
+            set;
+        }
+    }
+}
