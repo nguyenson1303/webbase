6b2936d8b1ee33bb8cc6a95d83a99a8638c7bfa2
sonnvpc <sonnv6460@co-well.com.vn>
2018-07-31 14:33:54 +0700

    sonnv add

---
+++ ApiBase/Models/DB/CatalogDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiBase.Models.DB
+{
+    public partial class CatalogDetail
+    {
+        public int CatalogDetailId { get; set; }
+        public int CatalogId { get; set; }
+        public string CategoryName { get; set; }
+        public string Link { get; set; }
+        public string Intro { get; set; }
+        public string MoreInfo { get; set; }
+        public string Lang { get; set; }
+        public string Keyword { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? ModifyDate { get; set; }
+    }
+}
